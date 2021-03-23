4080d11d3e5031efa3303db942a51fc60a6cf1be
Josh Martin (Work Laptop) <36969364+joshmartintv@users.noreply.github.com>
2021-03-23 13:02:33 -0500

    Initial commit

---
+++ app/src/main/java/com/joshmartindev/animationsamples/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.joshmartindev.animationsamples.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
